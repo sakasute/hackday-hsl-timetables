b03ae34f02131ef91c46417067fb93dcbd0b457c
Sakari Sutela <sakari.sutela@almamedia.fi>
2019-05-10 14:05:28 +0300

    Updates

---
+++ precache-manifest.15e7d04ba298dd05cdd683ee9b2b5cbc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e1725e463b3581e73f0bf5138e781d",
+    "url": "/hackday-hsl-timetables/index.html"
+  },
+  {
+    "revision": "44d82eef70beb843956d",
+    "url": "/hackday-hsl-timetables/static/js/2.e6adf91c.chunk.js"
+  },
+  {
+    "revision": "2a6fa7a26682257d546a",
+    "url": "/hackday-hsl-timetables/static/js/main.93102898.chunk.js"
+  },
+  {
+    "revision": "e0180ba3ad563a7fad03",
+    "url": "/hackday-hsl-timetables/static/js/runtime~main.2df6d5cc.js"
+  }
+]);
